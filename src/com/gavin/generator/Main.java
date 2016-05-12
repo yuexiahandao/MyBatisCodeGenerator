9ba0031b2afa78e336c8b2afb3a35bb2dfc689ef
gavin <yuexiahandao@gmail.com>
2016-05-12 12:41:22 +0800

    initial

---
+++ src/com/gavin/generator/Main.java
@@ -0,0 +1,53 @@
+package com.gavin.generator;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class Main {
+	public static void main(String[] argvs) {
+		System.out.println("begin to format file");
+		List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        String genCfg = "/mbgConfiguration.xml";
+        File configFile = new File(Main.class.getResource(genCfg).getFile());
+        // System.out.println(configFile.exists());
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = null;
+        
+        try {
+            config = cp.parseConfiguration(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+        
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = null;
+        try {
+            myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        
+        try {
+            myBatisGenerator.generate(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+	}
+}
